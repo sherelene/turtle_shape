2ea72359e1c29af2ecdc546cd5b93e5c55a5c343
sherelene <71803296+sherelene@users.noreply.github.com>
2022-02-22 09:23:57 -0800

    honeycomb

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-from turtle import *
 import turtle
-from turtle import Screen, Turtle
-import turtle
-MINIMUM_BRANCH_LENGTH = 5
+import random
 
-def build_tree(t, branch_length, shorten_by, angle):
-  if branch_length > MINIMUM_BRANCH_LENGTH:
-    t.forward(branch_length)
-    new_length = branch_length - shorten_by
-    t.left(angle)
-    build_tree(t, new_length, shorten_by, angle)
-    t.right(angle * 2)
-    build_tree(t, new_length, shorten_by, angle)
-    t.left(angle)
-    t.backward(branch_length)
-tree = turtle.Turtle()
-tree.hideturtle()
-tree.speed(200)
-tree.setheading(90)
-tree.color('purple')
-build_tree(tree, 50, 5, 30)
-turtle.mainloop()
+def draw_hexagon():
+  for i in range(6):
+    turtle.forward(50)
+    turtle.left(60)
+
+def draw_honeycomb():
+  for i in range(6):
+    draw_hexagon()
+    turtle.forward(50)
+    turtle.right(60)
+
+def figure_8():
+  turtle.circle(50)
+  turtle.circle(-50)
+
+# Activity 10 - Update the following mandala_flower
+# function so that each time the loop runs, the
+# turtle changes color to a new random color.
+# HINTS:
+# - Use turtle.color, and pass in three arguments
+#   into the method.
+# - Generate three random numbers, one for each
+#   value of RGB.
+# - Remember that we need to change the color
+#   each time we run the loop.
+def mandala_flower():
+
+  for i in range(35):
+
+    figure_8()
+
+    turtle.right(10)
+
+# Do not delete the following lines
+turtle.speed(0)
+turtle.shape("turtle")
+turtle.color("orange")
+
+draw_honeycomb()
+
+turtle.penup()
+turtle.goto(300, 300)
+turtle.pendown()
+
+mandala_flower()
