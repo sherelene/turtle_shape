a3a5aa0d0ada2b464c6e1428d0e8409e9c407154
sherelene <71803296+sherelene@users.noreply.github.com>
2022-02-22 10:42:18 -0800

    added spiral

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import turtle
 
+screen = turtle.Screen()
+screen.setup(500,500)
+screen.setworldcoordinates(-500,-500,500,500)
 art = turtle.Pen()
 art.speed(90)
+turtle.speed(90)
 turtle.bgcolor("black")
-#colors = ["red", "blue", "green", "orange", "purple", "pink"]
-colors = ["white","white","white","white","white",]
+colors = ["red", "blue", "green", "orange", "purple", "pink"]
+#colors = ["black","black","black","black","black",]
+#colors = ["white","white","white","white","white",]
 sides = 5
-distance = 3
+distance = 2
 angle = 45
 
-for x in range(360):
+
+
+for x in range(160):
   art.pencolor(colors[x%sides])
   art.forward(x*distance/sides + x)
   art.left(360/sides + angle)
   art.width(x*sides/360)
+
+
+
+for i in range(10,1800,6):
+    turtle.fd(i)
+    colors = ["red", "blue", "green", "orange", "purple", "pink"]
+    turtle.pencolor(colors[i % sides])
+    turtle.left(89.7)
+    turtle.speed(90)
